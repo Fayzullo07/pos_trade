4bfa5c63e7b11e3a3ec9746f14810748a00ee089
Fayzullo07 <jorayevfayzillo77@gmail.com>
2024-05-04 23:33:18 +0500

    chef

--- src/app/api/chef/route.tsx
+++ src/app/api/chef/route.tsx
@@ -6,7 +6,14 @@ export const GET = async (req: NextRequest) => {
     await connectMongoDB();
     try {
         let orders = await Chef.find({ isActive: true });
-        return NextResponse.json({ orders }, { status: 200 })
+        return NextResponse.json({ orders }, {
+            status: 200,
+            headers: {
+                'Cache-Control': 'public, s-maxage=1',
+                'CDN-Cache-Control': 'public, s-maxage=60',
+                'Vercel-CDN-Cache-Control': 'public, s-maxage=3600',
+            },
+        })
     } catch (error) {
         return NextResponse.json({ orders: [] }, { status: 500 })
     }

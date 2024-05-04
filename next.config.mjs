9459efe042a944b2eca9f99c47ed6b7f9d47f412
Fayzullo07 <jorayevfayzillo77@gmail.com>
2024-05-04 22:32:51 +0500

    server

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
-  async headers() {
-    return [
-      {
-        source: "/chef",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "s-maxage=1, stale-while-revalidate=59",
-          },
-        ],
-      },
-    ];
-  },
   images: {
     domains: ["t4.ftcdn.net", "cdn.filestackcontent.com"],
   },
